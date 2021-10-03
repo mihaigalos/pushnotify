d285e12d51c4dcfa54b25d067f2641a887d3b0f0
Mihai Galos <mihaigalos@gmail.com>
2021-10-03 10:53:31 +0200

    Dep-inject log_new into handler

--- src/push.sh
+++ src/push.sh
@@ -17,8 +17,9 @@ function pushnotify(){
 
 function handler_vpn(){
     log=$1
-    sudo journalctl --boot --lines=all | grep "ovpn-server[[]" | grep "Peer Connection Initiated" | tail -10 > ${log}_new
-    diff=$(diff ${log} ${log}_new)
+    log_new=$2
+    sudo journalctl --boot --lines=all | grep "ovpn-server[[]" | grep "Peer Connection Initiated" | tail -10 > ${log_new}
+    diff=$(diff ${log} ${log_new})
     [ $(echo $diff | wc -w) -gt 0 ] && pushnotify $(echo "$(hostname): New VPN connect: $(echo $diff | cut -d ' ' -f 8-9)") || echo "No new VPN connections."
 }
 
@@ -27,8 +28,9 @@ function main(){
     for handler in $(grep function $current_file | cut -d' ' -f2 | cut -d "(" -f1 | grep handler_);
     do
        log=${LOG_FILE_PREFIX}_${handler}
-       touch ${log} && $handler ${log}
-       cp ${log}_new ${log}
+       log_new=${log}_new
+       touch ${log} && $handler ${log} ${log_new}
+       cp ${log_new} ${log}
     done
     sleep $RUN_FREQUENCY_SECONDS # Poor man's cron, since :ro on journal files and cron cannot write
 }
